46ea9906887b1a05ad12a12b7c91ba3fe84dc790
罗巍 <86441933@qq.com>
2023-05-20 15:13:48 +0800

    chore: init

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import "./index.css";
+
+const container = document.getElementById("root") as HTMLElement;
+
+const root = createRoot(container);
+
+root.render(
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
+);
