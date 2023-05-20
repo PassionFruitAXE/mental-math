360428611f7b56ddb0d92df3728fcb89d641fde1
罗巍 <86441933@qq.com>
2023-05-20 22:13:32 +0800

    style: 新增玻璃拟态样式

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -15,14 +15,14 @@ const Header: FC<TProps> = ({ children, className }) => {
         aria-label="Global"
       >
         <div className="flex lg:flex-1 z-50">
-          <Link to="/" className="-m-1.5 p-1.5">
+          <Link to="/" className="flex -m-1.5 p-1.5">
             <img
               className="h-8 w-auto"
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt=""
             />
+            <strong className="ml-2 leading-8">天才口算平台</strong>
           </Link>
-          <strong className="ml-2 leading-8">天才口算平台</strong>
         </div>
         <div className="lg:flex lg:flex-1 lg:justify-end">{children}</div>
       </nav>
--- src/views/LoginPage/index.tsx
+++ src/views/LoginPage/index.tsx
@@ -18,81 +18,83 @@ const LoginPage: FC = () => {
           <span aria-hidden="true">&larr;</span> Return Home
         </Link>
       </Header>
-      <Main>
-        <div className="mx-auto max-w-2xl text-center">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            登录账号
-          </h2>
-          <p className="mt-2 text-lg leading-8 text-gray-600 text-auto">
-            请在此填写您的账号以及密码, 然后点击登录按钮登陆账号 <br />
-            如果您还没有账号请点击此处
-            <Link to="/register" className="font-semibold text-indigo-600">
-              注册
-            </Link>
-          </p>
+      <Main className="flex">
+        <div className="mx-auto p-12 rounded-3xl glass-xl">
+          <div className="mx-auto max-w-2xl text-center">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              登录账号
+            </h2>
+            <p className="mt-2 text-lg leading-8 text-gray-600 text-auto">
+              请在此填写您的账号以及密码, 然后点击登录按钮登陆账号 <br />
+              如果您还没有账号请点击此处
+              <Link to="/register" className="font-semibold text-indigo-600">
+                注册
+              </Link>
+            </p>
+          </div>
+          <form className="mx-auto mt-16 max-w-xl sm:mt-20">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  用户名
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  密码
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="password"
+                    id="password"
+                    autoComplete="password"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+              <div className="flex gap-x-4 sm:col-span-2">
+                <label
+                  className="text-sm leading-6 text-gray-600"
+                  id="switch-1-label"
+                >
+                  点击登录代表您已同意我们的
+                  <a href="#" className="font-semibold text-indigo-600">
+                    用户使用协议
+                  </a>
+                  .
+                </label>
+              </div>
+            </div>
+            <div className="mt-10">
+              <button
+                type="submit"
+                className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                onClick={() => {
+                  dispatch({ name: "luowei" });
+                  navigator("/");
+                }}
+              >
+                登录
+              </button>
+            </div>
+          </form>
         </div>
-        <form className="mx-auto mt-16 max-w-xl sm:mt-20">
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="username"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                用户名
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="username"
-                  id="username"
-                  autoComplete="username"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="password"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                密码
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="password"
-                  id="password"
-                  autoComplete="password"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div className="flex gap-x-4 sm:col-span-2">
-              <label
-                className="text-sm leading-6 text-gray-600"
-                id="switch-1-label"
-              >
-                点击登录代表您已同意我们的
-                <a href="#" className="font-semibold text-indigo-600">
-                  用户使用协议
-                </a>
-                .
-              </label>
-            </div>
-          </div>
-          <div className="mt-10">
-            <button
-              type="submit"
-              className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={() => {
-                dispatch({ name: "luowei" });
-                navigator("/");
-              }}
-            >
-              登录
-            </button>
-          </div>
-        </form>
       </Main>
     </Layout>
   );
